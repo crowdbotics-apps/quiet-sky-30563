a4d66ae42531aef9f91610b537c4a53025d2df7a
Crowdbotics <team@crowdbotics.com>
2021-09-15 06:06:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "quiet_sky_30563Identifier",
+  url: "https://quiet_sky_30563Identifier.botics.co",
+  api: "https://quiet_sky_30563Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
